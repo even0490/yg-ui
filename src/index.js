4127bc0fe4cf7be990a4b8c06d8f43fdb54a4b01
licheng <even0490@163.com>
2018-07-12 14:26:24 +0800

    add index wxport

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("pack");
+import Layout from "./layout/Layout.vue";
+
+Layout.install = function(Vue) {
+  Vue.component(Layout.name, Layout);
+};
+
+export { Layout as default, Layout };
