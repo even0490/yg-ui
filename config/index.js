261293c1cc47d2de46b9ee2df3357320ab3a47e8
licheng <even0490@163.com>
2018-07-17 11:41:47 +0800

    build docs

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
     // Paths
     assetsSubDirectory: "static",
-    assetsPublicPath: "/",
+    assetsPublicPath: "/yg-ui/",
     proxyTable: {},
 
     // Various Dev Server settings
--- examples/router/index.js
+++ examples/router/index.js
@@ -6,6 +6,7 @@ Vue.use(Router);
 
 export default new Router({
   mode: "history",
+  base: "yg-ui",
   routes: [
     {
       path: "/",
