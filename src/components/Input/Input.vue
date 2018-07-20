c32f92768b974db0006fb592c4d6935ff5b681f2
peiyuepeng <pyppeiyuepeng@hfbank.com.cn>
2018-07-20 17:37:30 +0800

    test input components

--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -1,8 +1,157 @@
 <template>
-  <input type="text" />
+  <div :class="[
+    type === 'textarea' ? 'el-textarea' : 'el-input',
+    size ? 'el-input--' + size : '',
+    {
+      'is-disabled': disabled,
+      'el-input-group': $slots.prepend || $slots.append,
+      'el-input-group--append': $slots.append,
+      'el-input-group--prepend': $slots.prepend
+    }
+  ]">
+    <template v-if="type !== 'textarea'">
+      <input
+        class="el-input__inner"
+        :type="type"
+        :name="name"
+        :placeholder="placeholder"
+        :disabled="disabled"
+        :readonly="readonly"
+        :maxlength="maxlength"
+        :minlength="minlength"
+        :autocomplete="autoComplete"
+        :autofocus="autofocus"
+        :min="min"
+        :max="max"
+        :step="step"
+        :form="form"
+        :value="currentValue"
+        ref="input"
+        @input="handleInput"
+        @focus="handleFocus"
+        @blur="handleBlur"
+      >
+    </template>
+  </div>
 </template>
 <script>
-export default {
-  name: "Yg-Input"
-};
+  import calcTextareaHeight from './calcTextareaHeight';
+  import merge from 'element-ui/src/utils/merge';
+
+  export default {
+    name: 'ElInput',
+
+    componentName: 'ElInput',
+
+    data() {
+      return {
+        currentValue: this.value
+      };
+    },
+
+    props: {
+      value: [String, Number],
+      placeholder: String,
+      size: String,
+      resize: String,
+      readonly: Boolean,
+      autofocus: Boolean,
+      icon: String,
+      disabled: Boolean,
+      type: {
+        type: String,
+        default: 'text'
+      },
+      name: String,
+      autosize: {
+        type: [Boolean, Object],
+        default: false
+      },
+      rows: {
+        type: Number,
+        default: 2
+      },
+      autoComplete: {
+        type: String,
+        default: 'off'
+      },
+      form: String,
+      maxlength: Number,
+      minlength: Number,
+      max: {},
+      min: {},
+      step: {},
+      validateEvent: {
+        type: Boolean,
+        default: true
+      },
+      onIconClick: Function
+    },
+
+    computed: {
+      validating() {
+        return this.$parent.validateState === 'validating';
+      },
+    },
+
+    watch: {
+      'value'(val, oldValue) {
+        this.setCurrentValue(val);
+      }
+    },
+
+    methods: {
+      handleBlur(event) {
+        this.$emit('blur', event);
+        if (this.validateEvent) {
+          this.dispatch('ElFormItem', 'el.form.blur', [this.currentValue]);
+        }
+      },
+      inputSelect() {
+        this.$refs.input.select();
+      },
+      resizeTextarea() {
+        if (this.$isServer) return;
+        var { autosize, type } = this;
+        if (!autosize || type !== 'textarea') return;
+        const minRows = autosize.minRows;
+        const maxRows = autosize.maxRows;
+
+        this.textareaCalcStyle = calcTextareaHeight(this.$refs.textarea, minRows, maxRows);
+      },
+      handleFocus(event) {
+        this.$emit('focus', event);
+      },
+      handleInput(event) {
+        const value = event.target.value;
+        this.$emit('input', value);
+        this.setCurrentValue(value);
+        this.$emit('change', value);
+      },
+      handleIconClick(event) {
+        if (this.onIconClick) {
+          this.onIconClick(event);
+        }
+        this.$emit('click', event);
+      },
+      setCurrentValue(value) {
+        if (value === this.currentValue) return;
+        this.$nextTick(_ => {
+          this.resizeTextarea();
+        });
+        this.currentValue = value;
+        if (this.validateEvent) {
+          this.dispatch('ElFormItem', 'el.form.change', [value]);
+        }
+      }
+    },
+
+    created() {
+      this.$on('inputSelect', this.inputSelect);
+    },
+
+    mounted() {
+      this.resizeTextarea();
+    }
+  };
 </script>
