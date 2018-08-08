8be6b56e0c729df496f3663388d96f43c11d808f
peiyuepeng <pyppeiyuepeng@hfbank.com.cn>
2018-08-08 10:51:04 +0800

    test

--- examples/components/HelloWorld.vue
+++ examples/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="main">
+  <div class="hello">
     <Layout></Layout>
-    <!--<h1>{{ msg }}</h1>-->
+    <h1>{{ msg }}</h1>
     <!--<h2>Essential Links</h2>
     <ul>
       <li>
