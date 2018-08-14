4fb99d1ced9d13602e7101226dda3a1f2fb1da5b
licheng <even0490@163.com>
2018-08-14 09:31:04 +0800

    add px-to-vw on postcss

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,10 +1,19 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-  "plugins": {
+  plugins: {
     "postcss-import": {},
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+    autoprefixer: {},
+    "postcss-px-to-viewport": {
+      viewportWidth: 375,
+      viewportHeight: 667,
+      unitPrecision: 5,
+      viewportUnit: "vw",
+      selectorBlackList: [".ignore"],
+      minPixelValue: 1,
+      mediaQuery: false
+    }
   }
-}
+};
