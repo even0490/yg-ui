4238c82f9710c0e4422472adbedbc2ef6aae87a0
licheng <even0490@163.com>
2018-11-26 16:43:25 +0800

    fix input bug

--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -132,6 +132,11 @@ export default {
   },
   mounted() {
     this.inputRegister && this.inputRegister(this, this.prop);
+  },
+  watch: {
+    type(val) {
+      this.types = val;
+    }
   }
 };
 </script>
